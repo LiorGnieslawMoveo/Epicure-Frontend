6a08bc961b3c0309a2a4684e6a391d49d7be0a92
Lior Gnieslaw <liorg@moveo.co.il>
2024-03-17 14:09:45 +0200

    first commit - header component

---
+++ epicure-frontend/src/components/Header.tsx
@@ -0,0 +1,39 @@
+import './Header.scss'; 
+import epicureLogo from '../assests/images/epicure-logo.svg';
+import hamburger from '../assests/images/hamburger.svg';
+import bagIcon from '../assests/images/bag-icon.svg';
+import magnifyingGlass from '../assests/images/magnifying-glass.svg';
+import userIcon from '../assests/images/user-icon.svg';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="hamburger">
+          <img src={hamburger} alt="Epicure Logo" />
+        </div>
+        <div className="left-container">
+            <div className="logo">
+                <img src={epicureLogo} alt="Epicure Logo" />
+                <div className="title">EPICURE</div>
+            </div>
+            <div className="navbar">
+                <ul>
+                    <li>Restaurants</li>
+                    <li>Chefs</li>
+                </ul>
+            </div>
+        </div>
+        <nav className="nav">
+          <ul>
+            <li><img src={magnifyingGlass} alt="Magnifying Glass"/></li>
+            <li><img src={userIcon} alt="User Icon"/></li>
+            <li><img src={bagIcon} alt="Bag Icon"/></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
