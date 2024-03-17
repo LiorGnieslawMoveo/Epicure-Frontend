eb6fa780a69399a93158784690007a37c8f7e119
Lior Gnieslaw <liorg@moveo.co.il>
2024-03-17 15:53:41 +0200

    naming fixes

--- epicure-frontend/src/components/Header.tsx
+++ epicure-frontend/src/components/Header.tsx
@@ -7,32 +7,34 @@ import userIcon from '../assests/images/user-icon.svg';
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="container">
-        <div className="hamburger">
-          <img src={hamburger} alt="Epicure Logo" />
+    <>
+      <header className="header">
+        <div className="container">
+          <div className="hamburger">
+            <img src={hamburger} alt="Epicure Logo" />
+          </div>
+          <div className="left-container">
+              <div className="logo">
+                  <img src={epicureLogo} alt="Epicure Logo" />
+                  <div className="title">EPICURE</div>
+              </div>
+              <div className="navbar-titles">
+                  <ul>
+                      <li>Restaurants</li>
+                      <li>Chefs</li>
+                  </ul>
+              </div>
+          </div>
+          <nav className="navbar-icons">
+            <ul>
+              <li><img src={magnifyingGlass} alt="Magnifying Glass"/></li>
+              <li><img src={userIcon} alt="User Icon"/></li>
+              <li><img src={bagIcon} alt="Bag Icon"/></li>
+            </ul>
+          </nav>
         </div>
-        <div className="left-container">
-            <div className="logo">
-                <img src={epicureLogo} alt="Epicure Logo" />
-                <div className="title">EPICURE</div>
-            </div>
-            <div className="navbar">
-                <ul>
-                    <li>Restaurants</li>
-                    <li>Chefs</li>
-                </ul>
-            </div>
-        </div>
-        <nav className="nav">
-          <ul>
-            <li><img src={magnifyingGlass} alt="Magnifying Glass"/></li>
-            <li><img src={userIcon} alt="User Icon"/></li>
-            <li><img src={bagIcon} alt="Bag Icon"/></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
