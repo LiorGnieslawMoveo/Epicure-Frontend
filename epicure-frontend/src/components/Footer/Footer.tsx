648c69d58f87a8f373709c55707fb1b3b35471a2
Lior Gnieslaw <liorg@moveo.co.il>
2024-03-18 10:10:26 +0200

    footer mobile+desktop

---
+++ epicure-frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className='footer'>
+        <div className='footer-links'>
+          <p>Contact Us</p>
+          <p>Term of Use</p>
+          <p>Privacy Policy</p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
