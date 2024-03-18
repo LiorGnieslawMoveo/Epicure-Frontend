3011df61eaa83757c8ea71b772343528f3db9a28
Lior Gnieslaw <liorg@moveo.co.il>
2024-03-18 18:08:56 +0200

    hero with desktop adjustments

--- epicure-frontend/src/App.tsx
+++ epicure-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import AboutUs from './components/AboutUs/AboutUs';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
 import IconsMeaning from './components/IconsMeaning/IconsMeaning';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <div className="App">
         <Header />
+        <Hero />
         <IconsMeaning />
         <AboutUs />
         <Footer />
