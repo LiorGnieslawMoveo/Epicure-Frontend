e602b82298fc98e3351e9bc2504c9fae01d99129
Lior Gnieslaw <liorg@moveo.co.il>
2024-03-18 14:25:40 +0200

    changes divs to links

--- epicure-frontend/src/App.tsx
+++ epicure-frontend/src/App.tsx
@@ -1,13 +1,17 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- epicure-frontend/src/components/Footer/Footer.tsx
+++ epicure-frontend/src/components/Footer/Footer.tsx
@@ -1,13 +1,21 @@
 import "./Footer.scss";
+import { Link } from 'react-router-dom';
+
 
 const Footer = () => {
+  const footerLinks = [
+    { text: "Contact Us", path: "/" },
+    { text: "Term of Use", path: "/" },
+    { text: "Privacy Policy", path: "/" }
+  ];
+
   return (
     <>
       <footer className='footer'>
         <div className='footer-links'>
-          <div>Contact Us</div>
-          <div>Term of Use</div>
-          <div>Privacy Policy</div>
+          {footerLinks.map((link, index) => (
+                  <Link className="link" key={index} to={link.path}>{link.text}</Link>
+              ))}
         </div>
       </footer>
     </>
