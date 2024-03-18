feca036e3c0a354e9b4e01a22686547085c0959d
Lior Gnieslaw <liorg@moveo.co.il>
2024-03-18 14:54:53 +0200

    updated desktop responsive

--- epicure-frontend/src/components/AboutUs/AboutUs.tsx
+++ epicure-frontend/src/components/AboutUs/AboutUs.tsx
@@ -7,19 +7,19 @@ const AboutUs = () => {
   return (
     <>
       <div className='about-us-container'>
-        <div className="logo-icons-container">
           <div className="logo-container">
             <img className='epicure-logo' src={epicureLogo} alt='Epicure Logo' />
           </div>
+        <div className="icons-descriptions-container">
           <div className="icons-container">
             <img className='googleplay-logo' src={googleIcon} alt='Google Play Logo' />
             <img className='appstore-logo' src={appleIcon} alt='App Store Logo' />
           </div>
-        </div>
-        <div className='descriptions-container'>
+          <div className='descriptions-container'>
           <h3 className='about-us-title'>ABOUT US:</h3>
-          <div className='about-us-description part-one'>{firstText}</div>
-          <div className='about-us-description part-two'>{secondText}</div>
+            <div className='about-us-description part-one'>{firstText}</div>
+            <div className='about-us-description part-two'>{secondText}</div>
+          </div>
         </div>
       </div>
     </>
