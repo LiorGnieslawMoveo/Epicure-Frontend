404ba61965c433d4241a5a7cd5b06c0730ecbb7f
Lior Gnieslaw <liorg@moveo.co.il>
2024-03-18 11:26:03 +0200

    about us component + fixes

--- epicure-frontend/src/components/AboutUs/AboutUs.tsx
+++ epicure-frontend/src/components/AboutUs/AboutUs.tsx
@@ -7,16 +7,16 @@ const AboutUs = () => {
   return (
     <>
       <div className='about-us-container'>
-          <div className="logo-container">
-            <img className='epicure-logo' src={epicureLogo} alt='Epicure Logo' />
-          </div>
+        <div className="logo-container">
+          <img className='epicure-logo' src={epicureLogo} alt='Epicure Logo' />
+        </div>
         <div className="icons-descriptions-container">
           <div className="icons-container">
             <img className='googleplay-logo' src={googleIcon} alt='Google Play Logo' />
             <img className='appstore-logo' src={appleIcon} alt='App Store Logo' />
           </div>
           <div className='descriptions-container'>
-          <h3 className='about-us-title'>ABOUT US:</h3>
+            <h3 className='about-us-title'>ABOUT US:</h3>
             <div className='about-us-description part-one'>{firstText}</div>
             <div className='about-us-description part-two'>{secondText}</div>
           </div>
