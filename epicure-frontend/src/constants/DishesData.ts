e240b6d0c38e1bd63194b44e092801aa43c5916e
Lior Gnieslaw <liorg@moveo.co.il>
2024-03-20 14:28:28 +0200

    data created

---
+++ epicure-frontend/src/constants/DishesData.ts
@@ -0,0 +1,30 @@
+import PadKiMao from './../assets/images/padkimao.svg'
+import TaMaLaKo from './../assets/images/ta-ma-la-ko.svg'
+import RedFarm from './../assets/images/red-farm.svg'
+
+
+const DishesData = {
+    title: "SIGNATURE DISH OF:",
+    cards: [
+        {
+            title: "Pad Ki Mao",
+            image: PadKiMao,
+            description: "Shrimps, Glass Noodles, Kemiri Nuts, Shallots, Lemon Grass, Magic Chili Brown Coconut",
+            price: 88,
+        },
+        {
+            title: "Ta Ma La Ko",
+            image: TaMaLaKo,
+            description: "Green Papaya, Mango, Chukka Chili, Mint, Kaffir lime, Cashew, Akaya Cham sauce",
+            price: 98,
+        },
+        {
+            title: "Red Farm",
+            image: RedFarm,
+            description: "Tofu, Spekkoek Peanuts, Spicy Manis, Pear Yakitori",
+            price: 98,
+        }
+    ],
+};
+
+export default DishesData;
---
+++ epicure-frontend/src/constants/RestaurantsData.ts
@@ -0,0 +1,66 @@
+import Claro from './../assets/images/claro.svg';
+import Onza from './../assets/images/onza.svg';
+import KabKem from './../assets/images/kab-kem.svg';
+import NithanThai from './../assets/images/nithan-thai.svg';
+import TigerLilly from './../assets/images/tiger-lily.svg';
+import Yapan from './../assets/images/yapan.svg';
+import Messa from './../assets/images/messa.svg';
+
+import OneStar from './../assets/images/1star.svg';
+import TwoStars from './../assets/images/2stars.svg';
+import ThreeStars from './../assets/images/3stars.svg';
+import FourStars from './../assets/images/4stars.svg';
+import FiveStars from './../assets/images/5stars.svg';
+
+import Spicy from './../assets/images/spicy-icon.svg';
+import PadKiMao from './../assets/images/padkimao.svg';
+
+const RestaurantsData = {
+    title: "POPULAR RESTAURANT IN EPICURE:",
+    cards: [
+        {
+            title: "Claro",
+            image: Claro,
+            subtitle: 'Ran Shmueli',
+            rating: FourStars,
+            className: "restaurant-card"
+        }, {
+            title: "Onza",
+            image: Onza,
+            rating: ThreeStars,
+            className: "restaurant-card"
+        }, {
+            title: "Kab Kem",
+            image: KabKem,
+            subtitle: "Yariv Malili",
+            rating: TwoStars,
+            className: "restaurant-card"
+        }, {
+            title: "Nitan Thai",
+            image: NithanThai,
+            subtitle: "Shahaf Shabtay",
+            rating: OneStar,
+            className: "restaurant-card"
+        }, {
+            title: "Tiger Lilly",
+            image: TigerLilly,
+            subtitle: "Yanir Green",
+            rating: FourStars,
+            className: "restaurant-card"
+        }, {
+            title: "Ya Pan",
+            image: Yapan,
+            subtitle: "Yuval Ben Moshe",
+            rating: TwoStars,
+            className: "restaurant-card"
+        }, {
+            title: "Messa",
+            image: Messa,
+            subtitle: "Aviv Moshe",
+            rating: OneStar,
+            className: "restaurant-card"
+        }
+    ]
+};
+
+export default RestaurantsData;
