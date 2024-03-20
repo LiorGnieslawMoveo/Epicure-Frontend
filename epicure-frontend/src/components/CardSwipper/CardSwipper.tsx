186a86666f64aadd3b34eeeebd8e8eb1b5da0c02
Lior Gnieslaw <liorg@moveo.co.il>
2024-03-20 15:34:47 +0200

    another dish added + all restaurant + card swipper changes for mobile (scrolling doesnt work yet)

--- epicure-frontend/src/components/CardSwipper/CardSwipper.tsx
+++ epicure-frontend/src/components/CardSwipper/CardSwipper.tsx
@@ -2,12 +2,14 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
+import { EffectFade } from 'swiper/modules';
 
 import { Autoplay, Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 
 import './CardSwipper.scss'
 import Card from '../Card/Card';
 import { cardProps } from '../../models/types';
+import AllRestaurants from './../../assets/images/all-restaurants.svg'
 
 const CardSwipper: React.FC<{ title: string, cards: cardProps[] }> = ({ title, cards }) => {
     return (
@@ -16,11 +18,11 @@ const CardSwipper: React.FC<{ title: string, cards: cardProps[] }> = ({ title, c
                 <h1 className='swipper-title'>{title}</h1>
                 <Swiper
                     className='swiper'
-                    modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+                    modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade]}
                     initialSlide={0}
                     spaceBetween={24}
                     slidesPerView={1.4}
-                    // navigation
+                    navigation
                     scrollbar={{ draggable: true }}
                     pagination
                     loop={true}
@@ -41,13 +43,19 @@ const CardSwipper: React.FC<{ title: string, cards: cardProps[] }> = ({ title, c
                     }}
                     watchOverflow={true}
                 >
-                    {cards.map((card, index) => (
-                        <SwiperSlide key={index}>
-                            <Card {...card} />
-                        </SwiperSlide>
-                    ))}
+                    {
+                        cards.map((card, index) => (
+                            <SwiperSlide key={index}>
+                                <Card {...card} />
+                            </SwiperSlide>
+                        ))
+                    }
                 </Swiper>
-            </div>
+                <div className='all-restaurants'>
+                    <span className='all-restaurants-title'>All Restaurants</span>
+                    <img src={AllRestaurants} alt='All Restaurants' className='all-resaurants-icon' />
+                </div>
+            </div >
         </>
     );
 };
--- epicure-frontend/src/constants/DishesData.ts
+++ epicure-frontend/src/constants/DishesData.ts
@@ -1,6 +1,11 @@
 import PadKiMao from './../assets/images/padkimao.svg'
 import TaMaLaKo from './../assets/images/ta-ma-la-ko.svg'
 import RedFarm from './../assets/images/red-farm.svg'
+import Hamburger from './../assets/images/hamburger-dish.svg'
+import Spicy from './../assets/images/spicy-icon.svg'
+import Vegan from './../assets/images/vegan-icon.svg'
+import Vegi from './../assets/images/vegi-icon.svg'
+
 
 
 const DishesData = {
@@ -10,19 +15,29 @@ const DishesData = {
             title: "Pad Ki Mao",
             image: PadKiMao,
             description: "Shrimps, Glass Noodles, Kemiri Nuts, Shallots, Lemon Grass, Magic Chili Brown Coconut",
+            iconMeaning: Spicy,
             price: 88,
         },
         {
             title: "Ta Ma La Ko",
             image: TaMaLaKo,
             description: "Green Papaya, Mango, Chukka Chili, Mint, Kaffir lime, Cashew, Akaya Cham sauce",
+            iconMeaning: Vegi,
             price: 98,
         },
         {
             title: "Red Farm",
             image: RedFarm,
             description: "Tofu, Spekkoek Peanuts, Spicy Manis, Pear Yakitori",
+            iconMeaning: Vegan,
             price: 98,
+        },
+        {
+            title: "Hamburger",
+            image: Hamburger,
+            description: "Buns, Meat, Fries",
+            iconMeaning: Spicy,
+            price: 78,
         }
     ],
 };
