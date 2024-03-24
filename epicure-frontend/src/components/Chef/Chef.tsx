f362a9c07baab35a044a3a8e41eab7a564c88d11
Lior Gnieslaw <liorg@moveo.co.il>
2024-03-24 11:25:18 +0200

    added chef component animation

--- epicure-frontend/src/components/Chef/Chef.tsx
+++ epicure-frontend/src/components/Chef/Chef.tsx
@@ -8,36 +8,40 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { Slide } from "react-awesome-reveal";
 
 const Chef = () => {
     let [chefFirstName] = ChefData.name.split(' ');
     chefFirstName = chefFirstName.toUpperCase();
     return (
         <div className='chef-container'>
-            <h3 className='title'>CHEF OF THE WEEK:</h3>
-            <div className='image-description-container'>
-                <div className='image-container'>
-                    <h3 className='chef-name'>{ChefData.name}</h3>
-                </div>
-                <p className='chef-description'>{ChefData.description}</p>
-            </div>
-            <div className='chef-restaurants-container'>
-                <h4 className='restaurants-list-title'>{chefFirstName}'S RESTAURANTS</h4>
-                <Swiper {...SwipperConfig(24)}>
-                    <div className='chefs-restaurants'>
-                        {ChefData.restaurants.map((restaurant, index) => (
-                            <SwiperSlide className='swiper-slide' key={index}>
-                                <Card className='restaurant-card' title={restaurant.title} image={restaurant.image}></Card>
-                            </SwiperSlide>
-                        ))}
-                    </div>
-                </Swiper>
-                <div className='all-restaurants'>
-                    <span className='all-restaurants-title'>All Restaurants</span>
-                    <img src={AllRestaurants} alt='All Restaurants' className='all-resaurants-icon' />
-                </div>
+            <Slide>
 
-            </div>
+                <h3 className='title'>CHEF OF THE WEEK:</h3>
+                <div className='image-description-container'>
+                    <div className='image-container'>
+                        <h3 className='chef-name'>{ChefData.name}</h3>
+                    </div>
+                    <p className='chef-description'>{ChefData.description}</p>
+                </div>
+                <div className='chef-restaurants-container'>
+                    <h4 className='restaurants-list-title'>{chefFirstName}'S RESTAURANTS</h4>
+                    <Swiper {...SwipperConfig(24)}>
+                        <div className='chefs-restaurants'>
+                            {ChefData.restaurants.map((restaurant, index) => (
+                                <SwiperSlide className='swiper-slide' key={index}>
+                                    <Card className='restaurant-card' title={restaurant.title} image={restaurant.image}></Card>
+                                </SwiperSlide>
+                            ))}
+                        </div>
+                    </Swiper>
+                    <div className='all-restaurants'>
+                        <span className='all-restaurants-title'>All Restaurants</span>
+                        <img src={AllRestaurants} alt='All Restaurants' className='all-resaurants-icon' />
+                    </div>
+
+                </div>
+            </Slide>
         </div>
     )
 }
