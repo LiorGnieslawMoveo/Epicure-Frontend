e42de5cbb072c9076da2504cb6bd2af7da9d75aa
Lior Gnieslaw <liorg@moveo.co.il>
2024-03-24 11:21:17 +0200

    chef style fix + added icon meaning animations

--- epicure-frontend/src/components/IconsMeaning/IconsMeaning.tsx
+++ epicure-frontend/src/components/IconsMeaning/IconsMeaning.tsx
@@ -1,5 +1,6 @@
 import './IconsMeaning.scss';
 import IconsMeaningInfo from '../../constants/IconsMeaningInfo';
+import { JackInTheBox } from 'react-awesome-reveal';
 
 
 const IconsMeaning = () => {
@@ -9,10 +10,12 @@ const IconsMeaning = () => {
                 <h2 className='icons-title'>THE MEANING OF OUR ICONS:</h2>
                 <div className='icons-container'>
                     {IconsMeaningInfo.map((icon, index) => (
-                        <div className='icon-item' key={index}>
-                            <img src={icon.image} alt={icon.name} />
-                            <h3>{icon.name}</h3>
-                        </div>
+                        <JackInTheBox key={index}>
+                            <div className='icon-item' key={index}>
+                                <img src={icon.image} alt={icon.name} />
+                                <h3>{icon.name}</h3>
+                            </div>
+                        </JackInTheBox>
                     ))}
                 </div>
             </div>
