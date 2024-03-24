418da10628138d98bb61a665a5ec7ad039467ff7
Lior Gnieslaw <liorg@moveo.co.il>
2024-03-24 16:08:13 +0200

    add animation to cardswipper and hero

--- epicure-frontend/src/components/Hero/Hero.tsx
+++ epicure-frontend/src/components/Hero/Hero.tsx
@@ -1,24 +1,28 @@
 import './Hero.scss';
 import SearchIcon from '../../assets/images/search-icon.svg'
+import { Slide } from "react-awesome-reveal";
+
 
 const Hero = () => {
     return (
         <>
             <div className='hero-container' >
-                <div className='hero-overlay'>
-                    <div className='hero-content'>
-                        <div className='hero-text'>Epicure works with the top chef restaurants in Tel Aviv</div>
-                        <div className='search-bar'>
-                            <button>
-                                <img src={SearchIcon} alt='Search Logo' />
-                            </button>
-                            <input
-                                type='text'
-                                placeholder='Search for restaurant cuisine, chef'
-                            />
+                <Slide>
+                    <div className='hero-overlay'>
+                        <div className='hero-content'>
+                            <div className='hero-text'>Epicure works with the top chef restaurants in Tel Aviv</div>
+                            <div className='search-bar'>
+                                <button>
+                                    <img src={SearchIcon} alt='Search Logo' />
+                                </button>
+                                <input
+                                    type='text'
+                                    placeholder='Search for restaurant cuisine, chef'
+                                />
+                            </div>
                         </div>
                     </div>
-                </div>
+                </Slide>
             </div>
         </>
     );
